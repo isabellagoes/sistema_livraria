2e64426bf1324f38ebaa3e533a620440f84286e9
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-29 19:59:17 -0300

    .

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 
 $hostname = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $database = "bd_livraria";
 
 $conexao = new mysqli($hostname, $username, $password, $database);
