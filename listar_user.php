f4bcae675861ad27505551706044fc4069d248b9
isabellagoes <isabellagoes.igs@gmail.com>
2024-05-20 21:26:34 -0300

    .

--- listar_user.php
+++ listar_user.php
@@ -1,7 +1,7 @@
 <?php include 'conexao.php'; ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
